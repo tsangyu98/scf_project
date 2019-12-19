a71a1634f2b218bb9ed957aa5a6d8b02e1d3cfbd
wu118 <MVPwu1314>
2019-12-19 18:51:37 +0800

    招聘接口——7.2

--- scf/apps/employment/urls.py
+++ scf/apps/employment/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
+from employment.views import *
 
 urlpatterns = [
-
+    url(r'^recruits/search/latest/$',NewPositionInfoView.as_view())
 ]
--- scf/apps/employment/views.py
+++ scf/apps/employment/views.py
@@ -1,3 +1,4 @@
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.shortcuts import render
 
@@ -6,8 +7,17 @@ from django.shortcuts import render
 
 
 # GET /recruits/search/latest/
+from employment.models import Recruit
+from employment.serializer import RecruitSerializer
+
+
 class NewPositionInfoView(APIView):
     """获取最新的招聘信息列表"""
 
     def get(self, request):
-        pass
+        queryset = Recruit.objects.all()
+
+        #序列化所有招聘信息
+        serializer = RecruitSerializer(queryset,many=True)
+
+        return Response(serializer.data)
