1dc36b6482fa5802752d4ea9c5e30b6fead56e3e
wu118 <MVPwu1314>
2019-12-19 18:26:01 +0800

    招聘接口

--- scf/apps/employment/views.py
+++ scf/apps/employment/views.py
@@ -1,3 +1,13 @@
+from rest_framework.views import APIView
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+# GET /recruits/search/latest/
+class NewPositionInfoView(APIView):
+    """获取最新的招聘信息列表"""
+
+    def get(self, request):
+        pass
