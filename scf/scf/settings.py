cbc6d523d41f8c6e34364ec445a70568113d760f
tsangyu98 <tsangyu1998@163.com>
2019-12-19 11:28:27 +0800

    '远端redis完成'

--- scf/scf/settings.py
+++ scf/scf/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/0",
+        "LOCATION": "redis://139.224.245.6:6379/0",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
